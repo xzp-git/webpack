7a779906f2d96795b87025fb4898a1ad976b4649
“xzpCSCKFWMS <l>
2022-01-04 21:10:36 +0800

    feat(bundle:bundle初始化)

--- src.基本概念/index.js
+++
@@ -1,9 +0,0 @@
-let title = require('./title')
-let content = require('./content.txt')
-
-console.log(title);
-console.log(content);
-console.log(process.env.NODE_ENV2);
-console.log('node_env',NODE_ENV2);
-
-
